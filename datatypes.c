bc079674a6527f3714d924548916a70fffb12396
gokul-s234 <gokulgokzz191@gmail.com>
2024-07-03 11:45:02 +0530

    first commit

---
+++ datatypes.c
@@ -0,0 +1,80 @@
+// datatype-
+
+// int-1234 -"%d"
+// float-2.3333333-%f
+// double-2.33333333333-%lf
+// char-'a','f'-%c
+// "hello "-%s
+// bool-true -%d
+
+// variabe-
+// syntax-datatyp variablename = value
+
+#include<stdio.h>
+int main()
+{
+    char b='g';
+    printf("%c\n",b);
+
+    float z=6.78;
+    printf("%f\n",z);
+
+    char m='h';
+    printf("%c\n",m);
+
+    double r=5.66;
+    printf("%lf\n",r);
+
+    char mn='y';
+    printf("%c\n",mn);
+
+    char v='x';
+    printf("%c\n",v);
+
+    int a=6;
+    printf("%d\n",a);
+
+    float j=4.7;
+    printf("%f\n",j);
+
+    int result=5>6;
+    printf("%d\n",result);
+
+    char e='n';
+    printf("%c\n",e);
+
+    float o=7.9;
+    printf("%f\n",o);
+
+    float u=6.8888;
+    printf("%lf\n",u);
+
+    int s=10;
+    printf("%d\n",s);
+
+    int answer=9<8;
+    printf("%d\n",result);
+
+    char jn='f';
+    printf("%c\n",jn);
+
+    float bv=6.99;
+    printf("%f",bv);
+
+    char du='d';
+    printf("%c\n",du);
+
+    
+
+
+    
+
+
+
+  
+
+
+
+}
+
+
---
+++ first.c
@@ -0,0 +1,9 @@
+// header file
+// main 
+
+#include<stdio.h>
+// #-preprocceso
+int main(){
+    // stmt;
+    printf("hello");
+}
